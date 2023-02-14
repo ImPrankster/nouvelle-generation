1d428b7d9696fb278462003b6f6a0cea8a3c9433
Ruofan Wong <meimprankster@gmail.com>
2023-02-14 20:44:49 +0800

    Change askai

--- app/discover/AskAi.tsx
+++ app/discover/AskAi.tsx
@@ -1,19 +1,44 @@
 "use client";
 
+import { useSupabase } from "@/components/SupabaseProvider";
+import { Session } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+import SignInCard from "@/components/SignInCard";
+
 const AskAi = () => {
+  const { supabase } = useSupabase();
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, [supabase.auth]);
+  console.log(session);
+
   return (
-    <div className="rounded-box mb-4 space-y-4 border border-base-300 bg-primary p-4 text-primary-content">
-      <div className="ml-4 text-xl font-medium">Ask AI</div>
-      <form onSubmit={() => {}} className="form-control w-full space-y-4">
-        <textarea
-          className="textarea-ghost textarea w-full text-xl font-bold"
-          placeholder="Cilck here to ask AI"
-        ></textarea>
-        <button className="btn-secondary btn self-end" type="submit">
-          Tell me!
-        </button>
-      </form>
-    </div>
+    <>
+      {!session ? (
+        <SignInCard />
+      ) : (
+        <div className="rounded-box mb-4 space-y-4 border border-base-300 bg-primary p-4 text-primary-content">
+          <div className="ml-4 text-xl font-medium">Ask AI</div>
+          <form onSubmit={() => {}} className="form-control w-full space-y-4">
+            <textarea
+              className="textarea-ghost textarea w-full text-xl font-bold"
+              placeholder="Cilck here to ask AI"
+            ></textarea>
+            <button className="btn-secondary btn self-end" type="submit">
+              Tell me!
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ components/SignInCard.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const SignInCard = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="card w-full max-w-md bg-base-100 font-bold shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">You are not signed in!</h2>
+          <Link href="/account/login" className="btn-primary btn self-end">
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignInCard;
